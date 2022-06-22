ff8eb94051cb1b3501ff5c94523e03f4d8348c0e
Catherine <c.nguyen18@yahoo.com>
2022-06-22 13:57:51 -0700

    Camera changes to the app

--- OverlayCameraApp/OverlayCameraApp/ViewController.swift
+++ OverlayCameraApp/OverlayCameraApp/ViewController.swift
@@ -7,21 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+        
+        imageView.backgroundColor = .secondarySystemBackground
+        s
+        button.backgroundColor = .systemTeal
+        button.setTitle("Take Picture", for: .normal)
 
+        button.setTitleColor(.white, for: .normal)
     }
-    @objc func addNewPerson() {
+    
+    @IBAction func didTapButton() {
         let picker = UIImagePickerController()
-        picker.allowsEditing = true
+        picker.sourceType = .camera
         picker.delegate = self
         present(picker, animated: true)
     }
-    
-
-
 }
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    picker.dismiss(animated: true, completion: nil)
+}
+    func imagePickerController(_ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as?
+                UIImage else {
+            return
+        }
+        imageView.image = image
+    }
+}
+
+
+
 
