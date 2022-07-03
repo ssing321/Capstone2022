4c4b1992b7411c26bb53bc7cf7115489eee9585a
ssing321 <onlyshaswatsingh09@yahoo.com>
2022-07-03 13:15:13 -0700

    fix the live view and chage opacity

--- TakingPictures/TakingPictures/uploadView.swift
+++ TakingPictures/TakingPictures/uploadView.swift
@@ -8,13 +8,34 @@
 import UIKit
 
 
-class uploadView: UIViewController {
+class uploadView: UIViewController,UIImagePickerControllerDelegate ,UINavigationControllerDelegate {
 
     @IBOutlet weak var uploadImage: UIImageView!
     var newImageUploadStore: UIImage!
     
+    @IBAction func startCameraLiveOverlay(_sender : UIBarButtonItem) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
+              let picker = UIImagePickerController()
+              picker.sourceType = .camera
+              picker.delegate = self
+              picker.cameraOverlayView = uploadImage
+              self.present(picker, animated: true)
+            }
+    }
+    func imagePickerController(_ picker: UIImagePickerController,
+                      didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+       let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        uploadImage.contentMode = .scaleAspectFit
+        uploadImage.image = image
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+         
+        self.dismiss(animated: true)
+         
+        }
+    
         override func viewDidLoad() {
         super.viewDidLoad()
         uploadImage.image = newImageUploadStore
+        uploadImage.alpha = 0.3
     }
 }
