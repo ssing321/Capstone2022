9f6bed02d5f344f3760cb31beaa544cd6ff6e698
ssing321 <onlyshaswatsingh09@gmail.com>
2022-08-02 08:10:34 +0530

    replaced camera with custom camera [WIP]

---
+++ TakingPictures/TakingPictures/customCamera/liveView.swift
@@ -0,0 +1,79 @@
+//
+//  liveView.swift
+//  TakingPictures
+//
+//  Created by Shaswat Singh on 8/2/22.
+//
+
+import UIKit
+import AVKit
+
+class liveView: UIViewController, AVCapturePhotoCaptureDelegate {
+    @IBOutlet weak var previewView: UIView!
+    @IBOutlet weak var captureImageView: UIImageView!
+    var captureSession: AVCaptureSession!
+    var stillImageOutput: AVCapturePhotoOutput!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    var newImage: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .medium
+        guard let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
+            else {
+                print("Unable to access back camera!")
+                return
+        }
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera)
+            stillImageOutput = AVCapturePhotoOutput()
+            if captureSession.canAddInput(input) && captureSession.canAddOutput(stillImageOutput) {
+                captureSession.addInput(input)
+                captureSession.addOutput(stillImageOutput)
+                setupLivePreview()
+            }
+        }
+        catch let error  {
+            print("Error Unable to initialize back camera:  \(error.localizedDescription)")
+        }
+    }
+    
+    
+    
+    func setupLivePreview() {
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer.videoGravity = .resizeAspect
+        videoPreviewLayer.connection?.videoOrientation = .portrait
+        previewView.layer.addSublayer(videoPreviewLayer)
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.captureSession.startRunning()
+            DispatchQueue.main.async {
+                self.videoPreviewLayer.frame = self.previewView.bounds
+            }
+        }
+    }
+    
+    @IBAction func didTakePhoto(_sender : UIBarButtonItem) {
+        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
+        stillImageOutput.capturePhoto(with: settings, delegate: self)
+    }
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        
+        guard let imageData = photo.fileDataRepresentation()
+            else { return }
+        
+        let image = UIImage(data: imageData)
+        captureImageView.image = image
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.captureSession.stopRunning()
+    }
+}
--- TakingPictures/TakingPictures/uploadView.swift
+++ TakingPictures/TakingPictures/uploadView.swift
@@ -13,29 +13,34 @@ class uploadView: UIViewController,UIImagePickerControllerDelegate ,UINavigation
     @IBOutlet weak var uploadImage: UIImageView!
     var newImageUploadStore: UIImage!
     
-    @IBAction func startCameraLiveOverlay(_sender : UIBarButtonItem) {
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
-              let picker = UIImagePickerController()
-              picker.sourceType = .camera
-              picker.delegate = self
-              picker.cameraOverlayView = uploadImage
-              self.present(picker, animated: true)
-            }
-    }
-    func imagePickerController(_ picker: UIImagePickerController,
-                      didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-       let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-        uploadImage.contentMode = .scaleAspectFit
-        uploadImage.image = image
-        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-         
-        self.dismiss(animated: true)
-         
-        }
+    
+//    @IBAction func startCameraLiveOverlay(_sender : UIBarButtonItem) {
+//        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
+//              let picker = UIImagePickerController()
+//              picker.sourceType = .camera
+//              picker.delegate = self
+//              picker.cameraOverlayView = uploadImage
+//              self.present(picker, animated: true)
+//            }
+//    }
+    
+//    func imagePickerController(_ picker: UIImagePickerController,
+//                      didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//       let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+//        uploadImage.contentMode = .scaleAspectFit
+//        uploadImage.image = image
+//        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+//
+//        self.dismiss(animated: true)
+//        }
     
         override func viewDidLoad() {
         super.viewDidLoad()
         uploadImage.image = newImageUploadStore
-        uploadImage.alpha = 0.3
+        uploadImage.alpha = 0.5
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! liveView
+        vc.newImage = self.uploadImage.image
     }
 }
