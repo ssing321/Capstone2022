ddb52a2e770a08750d58a7222b5adb9d575c4fbb
ssing321 <onlyshaswatsingh09@gmail.com>
2022-09-16 09:29:10 -0700

    added opacity slider feature

--- TakingPictures/TakingPictures/uploadView.swift
+++ TakingPictures/TakingPictures/uploadView.swift
@@ -12,30 +12,36 @@ class uploadView: UIViewController,UIImagePickerControllerDelegate ,UINavigation
 
     @IBOutlet weak var uploadImage: UIImageView!
     var newImageUploadStore: UIImage!
+    var finalvalue: Int = 0
+    
+    @IBAction func sliderDidSlide(_ sender: UISlider) {
+        let value = sender.value
+        uploadImage.alpha = CGFloat(value)
+        finalvalue = Int(value)
+    }
     
     @IBAction func startCameraLiveOverlay(_sender : UIBarButtonItem) {
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
-              let picker = UIImagePickerController()
-              picker.sourceType = .camera
-              picker.delegate = self
-              picker.cameraOverlayView = uploadImage
-              self.present(picker, animated: true)
-            }
+            let picker = UIImagePickerController()
+            picker.sourceType = .camera
+            picker.delegate = self
+            picker.cameraOverlayView = uploadImage
+            self.present(picker, animated: true)
+        }
     }
-    func imagePickerController(_ picker: UIImagePickerController,
-                      didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-       let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+    
+    func imagePickerController(_ picker: UIImagePickerController,didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
         uploadImage.contentMode = .scaleAspectFit
         uploadImage.image = image
         UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-         
         self.dismiss(animated: true)
-         
-        }
-    
-        override func viewDidLoad() {
+    }
+
+    override func viewDidLoad() {
         super.viewDidLoad()
         uploadImage.image = newImageUploadStore
-        uploadImage.alpha = 0.3
+        uploadImage.alpha = 0.5
     }
 }
