deb3d70154518afe9fa82026a20d809ece9a477c
ssing321 <onlyshaswatsingh09@gmail.com>
2022-10-14 17:49:12 -0700

    slider + storyboard[errors]

--- TakingPictures/TakingPictures/ViewController.swift
+++ TakingPictures/TakingPictures/ViewController.swift
@@ -10,8 +10,6 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var button: UIButton!
-    
-    
     var newImageStore: UIImage!
     
     override func viewDidLoad() {
@@ -44,8 +42,19 @@ class ViewController: UIViewController {
         let vc = segue.destination as! uploadView
         vc.newImageUploadStore = self.newImageStore
     }
+    
+    func fixOrientation(img: UIImage) -> UIImage {
+        if (img.imageOrientation == .up) {
+            return img
+        }
+        UIGraphicsBeginImageContextWithOptions(img.size, false, img.scale)
+        let rect = CGRect(x: 0, y: 0, width: img.size.width, height: img.size.height)
+        img.draw(in: rect)
+        let normalizedImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return normalizedImage
+    }
 }
-
 
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
@@ -61,7 +70,7 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
             return
         }
         imageView.image = image
+        imageView.image = fixOrientation(img: imageView.image!)
     }
-
 }
 
--- TakingPictures/TakingPictures/uploadView.swift
+++ TakingPictures/TakingPictures/uploadView.swift
@@ -9,36 +9,43 @@ import UIKit
 
 
 class uploadView: UIViewController,UIImagePickerControllerDelegate ,UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var uploadImage: UIImageView!
     var newImageUploadStore: UIImage!
     var finalvalue: Int = 0
+    var imageStore: UIImage!
     
     @IBAction func sliderDidSlide(_ sender: UISlider) {
         let value = sender.value
-        uploadImage.alpha = CGFloat(value)
+        uploadImage.alpha = CGFloat(exactly: value)!
         finalvalue = Int(value)
+        imageStore = uploadImage.image
     }
     
     @IBAction func startCameraLiveOverlay(_sender : UIBarButtonItem) {
+        let picker = UIImagePickerController()
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
-            let picker = UIImagePickerController()
             picker.sourceType = .camera
             picker.delegate = self
+            picker.allowsEditing = true
             picker.cameraOverlayView = uploadImage
-            self.present(picker, animated: true)
+            picker.cameraOverlayView?.frame(forAlignmentRect: CGRect(x: 0, y: 0, width: (uploadImage.image?.size.width)!, height: (uploadImage.image?.size.height)!))
+            self.present(picker, animated: true, completion: nil)
         }
     }
     
-    func imagePickerController(_ picker: UIImagePickerController,didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
-    {
-        let image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-        uploadImage.contentMode = .scaleAspectFit
-        uploadImage.image = image
-        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
-        self.dismiss(animated: true)
+    func imagePickerController(_ picker: UIImagePickerController,didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        if let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            self.uploadImage.contentMode = .scaleAspectFit
+            self.uploadImage.image = image
+            dismiss(animated: true)
+                {
+            self.performSegue(withIdentifier: "goToFinalView", sender: self)
+                }
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         uploadImage.image = newImageUploadStore
