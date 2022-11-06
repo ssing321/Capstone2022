c9330c7492e99d85c9f3ae7ad40d3ee6b66cabcd
ssing321 <onlyshaswatsingh09@gmail.com>
2022-11-05 18:05:32 -0700

    text for slider + added app icon

--- TakingPictures/TakingPictures/finalView/firstFinal.swift
+++ TakingPictures/TakingPictures/finalView/firstFinal.swift
@@ -62,7 +62,7 @@ class firstFinal: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.setHidesBackButton(false, animated: false)
+//        self.navigationItem.setHidesBackButton(false, animated: false)
         firstImage.image = image
     }
 }
